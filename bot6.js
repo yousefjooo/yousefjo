4b7bcaea01192473657fae2b3a1d98bb77c814d9
yousefjooo <44672126+yousefjooo@users.noreply.github.com>
2018-11-10 14:57:15 +0200

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
 
 client.on('message', message => {
