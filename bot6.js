07334207a91f262a7baed0367dfad75120c542ac
yousefjooo <44672126+yousefjooo@users.noreply.github.com>
2018-11-13 14:44:23 +0200

    Update bot6.js

--- bot6.js
+++ bot6.js
@@ -1,51 +1,44 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
- 
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
+const devs = ["433368297057288220"];
+const adminprefix = ["j"];
 client.on('message', message => {
-    var prefix = "jst";
-    
-      if (!message.content.startsWith(prefix)) return;
-      var args = message.content.split(' ').slice(1);
-      var argresult = args.join(' ');
-      if (message.author.id == 433368297057288220) return;
-    
-    
-    if (message.content.startsWith(prefix + 'playing')) {
-    if (message.author.id !== '433368297057288220') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+
+  if (message.content.startsWith(adminprefix + 'pl')) {
     client.user.setGame(argresult);
-        message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
-    } else
-    
-     
-    if (message.content.startsWith(prefix + 'streem')) {
-    if (message.author.id !== '433368297057288220') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-    client.user.setGame(argresult, "http://twitch.tv/HA");
-        message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
-    } else
-    
-    if (message.content.startsWith(prefix + 'setname')) {
-    if (message.author.id !== '433368297057288220') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-      client.user.setUsername(argresult).then
-          message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
-      return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
-    } else
-        
-    if (message.content.startsWith(prefix + 'setavatar')) {
-    if (message.author.id !== '433368297057288220') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-    client.user.setAvatar(argresult);
-        message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-    } else
-    
-    
-    if (message.content.startsWith(prefix + 'watching')) {
-    if (message.author.id !== '433368297057288220') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
-        client.user.setActivity(argresult, {type : 'watching'});
-     message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
-    }
-    
-     });
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "lev")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'w')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'l')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح **`)
+  }
+  if (message.content.startsWith(adminprefix + 'name')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**✅ تم تغير الاسم بنجاح ${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'avatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**✅ تم تغير الصور بنجاح ${argresult}** `);
+}
+});//hamo
 
-
-client.login(process.env.BOT_TOKEN6);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
